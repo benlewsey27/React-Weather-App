a410f8de8a86e1444605bd67fd880713167fb86c
Benjamin Lewsey <Benjamin.Lewsey@dvla.gov.uk>
2020-01-21 14:06:03 +0000

    convert weather-card to component

--- src/components/Weather-Card.js
+++ src/components/Weather-Card.js
@@ -1,22 +1,24 @@
 import React from "react";
 
-const WeatherCard = props => {
-  return (
-    <div>
-      {props.cards.map(card => (
-        <div key={card.id}>
-          <div className="card rounded-0">
-            <div className="card-body">
-              <u>
-                <h5 className="card-title">{card.title}</h5>
-              </u>
-              <p className="card-text">{card.body}</p>
+class WeatherCard extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.cards.map(card => (
+          <div key={card.id}>
+            <div className="card rounded-0">
+              <div className="card-body">
+                <u>
+                  <h5 className="card-title">{card.title}</h5>
+                </u>
+                <p className="card-text">{card.body}</p>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
-  );
-};
+        ))}
+      </div>
+    );
+  }
+}
 
 export default WeatherCard;
