7ad622b6cb4188ff121011925f7ed3787b7dfae3
Benjamin Lewsey <Benjamin.Lewsey@dvla.gov.uk>
2020-01-21 14:09:04 +0000

    add delete button

--- src/components/Weather-Card.js
+++ src/components/Weather-Card.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 class WeatherCard extends React.Component {
+  sendData = (title) => {
+    this.props.removeCard(title);
+  }
+  
   render() {
     return (
       <div>
@@ -12,6 +16,7 @@ class WeatherCard extends React.Component {
                   <h5 className="card-title">{card.title}</h5>
                 </u>
                 <p className="card-text">{card.body}</p>
+                <button onClick={() => this.sendData(card.id)} className="btn-danger rounded">Delete</button>
               </div>
             </div>
           </div>
