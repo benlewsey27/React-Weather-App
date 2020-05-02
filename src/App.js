465b40fa3173928442bad1095a5fc36d542a9bd5
Benjamin Lewsey <Benjamin.Lewsey@dvla.gov.uk>
2020-05-03 00:10:11 +0100

    refactor: move weather app into own component

--- src/App.js
+++ src/App.js
@@ -1,72 +1,13 @@
 import React from "react";
-import WeatherCard from "./components/Weather-Card";
-import axios from 'axios';
-import uuid from 'uuid';
+import WeatherComponent from './components/WeatherComponent'
 import './App.css'
 
+
 class App extends React.Component {
-  state = {
-    cards: []
-  };
-
-  removeCard(id){
-    let newArray = this.state.cards.filter((item) => item.id !== id);
-
-    this.setState({
-      cards: newArray
-    })
-  }
-
-  async getTempature(place){
-    // Get API_Key from https://openweathermap.org/api
-    const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
-    
-    const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${place}&APPID=${apiKey}`);
-
-    const temp = Math.round((response.data.main.temp - 273.15)*10)/10
-    return temp;
-  }
-
-  formatTitle(title){
-    return title.charAt(0).toUpperCase() + title.slice(1);
-  }
-
-  async addElement() {
-    const inputForm = document.getElementById("input");
-    const errorMessage = document.getElementById("error");
-
-    if (inputForm.value) {
-      try{
-        const title = this.formatTitle(inputForm.value);
-        const temp = await this.getTempature(title);
-      
-        const body = `The tempature is ${temp} degrees celcius.`
-
-        inputForm.value = "";
-        errorMessage.style.display = 'none';
-
-        const newArray = this.state.cards.concat({ id: uuid.v1(), title: title, body: body });
-        this.setState({
-          cards: newArray
-        });
-      }catch(err){
-        console.log(err);
-        errorMessage.style.display = 'block';
-        inputForm.value = "";
-      }
-    }
-  }
-
-  render() {
+  render(){
     return (
       <div>
-        <center className='jumbotron mb-0 rounded-0'>
-          <h1 className="mt-4 mb-4">React Weather</h1>
-          <input type="text" id="input"></input>
-          <button className='mb-4 ml-2 rounded' onClick={() => {this.addElement()}}>Submit</button>
-          <p id="error" className='mb-0'>Error: Unknown Place</p>
-        </center>
-        <WeatherCard cards={this.state.cards} removeCard={this.removeCard.bind(this)} />
+        <WeatherComponent/>
       </div>
     );
   }
