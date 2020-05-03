518a5a6699964c9ae9b589e68719bfb1c614931e
Benjamin Lewsey <Benjamin.Lewsey@dvla.gov.uk>
2020-05-03 12:44:07 +0100

    style: change navbar colour to success

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function NavBar(){
   return(
-    <nav className="navbar navbar-dark bg-primary fixed-top">
+    <nav className="navbar navbar-dark bg-success fixed-top">
       <h1 className="navbar-brand">React Weather</h1>
     </nav>
   );
