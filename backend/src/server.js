73f4016cbd0979a15da14318d784b4ed62fe20f4
benlewsey27 <benlewsey@sky.com>
2022-10-25 20:55:51 +0100

    rewrite backend using go

--- backend/src/server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const routes = require('./routes');
-
-const app = express();
-
-app.use('/', routes);
-app.listen(3001, () => console.log("Backend listening on port 3001..."));
--- scripts/release.sh
+++ scripts/release.sh
@@ -2,11 +2,8 @@
 
 echo $GITHUB_TOKEN | docker login ghcr.io -u benlewsey27 --password-stdin
 
-backendVersion=$(jq -r '.version' backend/package.json)
+backendVersion=$(cat backend/VERSION)
 frontendVersion=$(jq -r '.version' frontend/package.json)
-
-docker build -t ghcr.io/benlewsey27/react-weather-backend:$backendVersion ./backend 
-docker build -t ghcr.io/benlewsey27/react-weather-frontend:$frontendVersion ./frontend 
 
 docker push ghcr.io/benlewsey27/react-weather-backend:$backendVersion
 docker push ghcr.io/benlewsey27/react-weather-frontend:$frontendVersion
