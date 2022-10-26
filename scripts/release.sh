96232848645ef912c711d2722f828f3d33ffa451
benlewsey27 <benlewsey@sky.com>
2022-10-26 21:15:04 +0100

    rename docker images to react-weather and react-weather-api

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,11 +2,11 @@
 
 echo $GITHUB_TOKEN | docker login ghcr.io -u benlewsey27 --password-stdin
 
-backendVersion=$(cat backend/VERSION)
 frontendVersion=$(jq -r '.version' frontend/package.json)
+backendVersion=$(cat backend/VERSION)
 
-docker push ghcr.io/benlewsey27/react-weather-backend:$backendVersion
-docker push ghcr.io/benlewsey27/react-weather-frontend:$frontendVersion
+docker push ghcr.io/benlewsey27/react-weather:$frontendVersion
+docker push ghcr.io/benlewsey27/react-weather-api:$backendVersion
 
-docker rmi ghcr.io/benlewsey27/react-weather-backend:$backendVersion
-docker rmi ghcr.io/benlewsey27/react-weather-frontend:$frontendVersion
+docker rmi ghcr.io/benlewsey27/react-weather:$frontendVersion
+docker rmi ghcr.io/benlewsey27/react-weather-api:$backendVersion
