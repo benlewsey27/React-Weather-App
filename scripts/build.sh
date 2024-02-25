e0242660db6d95de2e6edb42e93d22853d4c9ce7
benlewsey27 <benlewsey@sky.com>
2024-02-25 17:17:44 +0000

    add workflows to build images

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-frontendVersion=$(jq -r '.version' frontend/package.json)
-backendVersion=$(cat backend/VERSION)
-
-docker build -t ghcr.io/benlewsey27/react-weather:$frontendVersion ./frontend 
-docker build -t ghcr.io/benlewsey27/react-weather-api:$backendVersion ./backend 
--- scripts/release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo $GITHUB_TOKEN | docker login ghcr.io -u benlewsey27 --password-stdin
-
-frontendVersion=$(jq -r '.version' frontend/package.json)
-backendVersion=$(cat backend/VERSION)
-
-docker push ghcr.io/benlewsey27/react-weather:$frontendVersion
-docker push ghcr.io/benlewsey27/react-weather-api:$backendVersion
