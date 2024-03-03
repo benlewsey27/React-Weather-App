18c62e467d8c58ea698a935f98f21c1750883fc0
benlewsey27 <benlewsey@sky.com>
2024-03-03 13:01:32 +0000

    WIP: add initial tests

---
+++ tests/script.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  vus: 10,
+  duration: '1m'
+};
+
+export default function() {
+  http.get('https://react-weather.127.0.0.1.nip.io:8080/api/get-temp/Cardiff');
+  sleep(1);
+}
